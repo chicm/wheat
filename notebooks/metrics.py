3ba1dc98245a3d23bab8f41881ee70a04fc99137
chicm <idv2005@gmail.com>
2020-05-25 19:20:29 +0000

    updates

--- notebooks/metrics.py
+++ notebooks/metrics.py
@@ -9,7 +9,18 @@ import matplotlib.pyplot as plt
 
 from numba import jit
 from typing import List, Union, Tuple
-from tqdm import tqdm
+from tqdm.notebook import tqdm
+
+import pandas as pd
+import numpy as np
+import numba
+import re
+import cv2
+import ast
+import matplotlib.pyplot as plt
+
+from numba import jit
+from typing import List, Union, Tuple
 
 
 @jit(nopython=True)
@@ -55,6 +66,7 @@ def calculate_iou(gt, pr, form='pascal_voc') -> float:
     )
 
     return overlap_area / union_area
+
 
 @jit(nopython=True)
 def find_best_match(gts, pred, pred_idx, threshold = 0.5, form = 'pascal_voc', ious=None) -> int:
@@ -168,82 +180,32 @@ def calculate_image_precision(gts, preds, thresholds = (0.5, ), form = 'coco') -
 
     return image_precision
 
+def show_result(sample_id, preds, gt_boxes):
+    sample = cv2.imread(f'{TRAIN_ROOT_PATH}/{sample_id}.jpg', cv2.IMREAD_COLOR)
+    sample = cv2.cvtColor(sample, cv2.COLOR_BGR2RGB)
 
+    fig, ax = plt.subplots(1, 1, figsize=(16, 8))
 
-#### make predictions
-def make_predictions(model, images, score_threshold=0.1):
-    # model: DetBenchEval
-    images = torch.stack(images).cuda().float()
-    predictions = []
-    with torch.no_grad():
-        det = model(images, torch.tensor([1]*images.shape[0]).float().cuda())
-        for i in range(images.shape[0]):
-            boxes = det[i].detach().cpu().numpy()[:,:4]    
-            scores = det[i].detach().cpu().numpy()[:,4]
-            indexes = np.where(scores > score_threshold)[0]
-            boxes = boxes[indexes]
-            boxes[:, 2] = boxes[:, 2] + boxes[:, 0]
-            boxes[:, 3] = boxes[:, 3] + boxes[:, 1]
-            predictions.append({
-                'boxes': boxes[indexes],
-                'scores': scores[indexes],
-            })
-    return [predictions]
+    for pred_box in preds:
+        cv2.rectangle(
+            sample,
+            (pred_box[0], pred_box[1]),
+            (pred_box[2], pred_box[3]),
+            (220, 0, 0), 2
+        )
 
-def run_wbf(predictions, image_index, image_size=512, iou_thr=0.55, skip_box_thr=0.45, weights=None):
-    #print(list(predictions[0][image_index].keys()))
-    boxes = [(prediction[image_index]['boxes']/(image_size-1)).tolist()  for prediction in predictions]
-    scores = [prediction[image_index]['scores'].tolist()  for prediction in predictions]
-    labels = [np.ones(prediction[image_index]['scores'].shape[0]).tolist() for prediction in predictions]
-    boxes, scores, labels = weighted_boxes_fusion(boxes, scores, labels, weights=None, iou_thr=iou_thr, skip_box_thr=skip_box_thr)
-    boxes = boxes*(image_size-1)
-    return boxes, scores, labels
+    for gt_box in gt_boxes:    
+        cv2.rectangle(
+            sample,
+            (gt_box[0], gt_box[1]),
+            (gt_box[2], gt_box[3]),
+            (0, 0, 220), 2
+        )
 
-def predict_test_set(model, data_loader):
-    results = []
-
-    for images, image_ids in data_loader:
-        predictions = make_predictions(model, images)
-        for i, image in enumerate(images):
-            boxes, scores, labels = run_wbf(predictions, image_index=i)
-            boxes = (boxes*2).astype(np.int32).clip(min=0, max=1023)
-            image_id = image_ids[i]
-
-            boxes[:, 2] = boxes[:, 2] - boxes[:, 0] # coco
-            boxes[:, 3] = boxes[:, 3] - boxes[:, 1] # coco
-
-            result = {
-                'image_id': image_id,
-                'PredictionString': format_prediction_string(boxes, scores)
-            }
-            results.append(result)
+    ax.set_axis_off()
+    ax.imshow(sample)
+    ax.set_title("RED: Predicted | BLUE - Ground-truth")
     
-    return results
-
-def predict_eval_set(model, validation_loader):
-    all_predictions = []
-    #for fold_number in range(5):
-    if True:
-        for images, targets, image_ids in tqdm(validation_loader, total=len(validation_loader)):
-            with torch.no_grad():
-                images = torch.stack(images)
-                images = images.cuda().float()
-                det = model(images, torch.tensor([1]*images.shape[0]).float().cuda())
-
-                for i in range(images.shape[0]):
-                    boxes = det[i].detach().cpu().numpy()[:,:4]    
-                    scores = det[i].detach().cpu().numpy()[:,4]
-                    boxes[:, 2] = boxes[:, 2] + boxes[:, 0]
-                    boxes[:, 3] = boxes[:, 3] + boxes[:, 1]
-                    all_predictions.append({
-                        'pred_boxes': (boxes*2).clip(min=0, max=1023).astype(int),
-                        'scores': scores,
-                        'gt_boxes': (targets[i]['boxes'].cpu().numpy()*2).clip(min=0, max=1023).astype(int),
-                        'image_id': image_ids[i],
-                    })
-
-    return all_predictions
-
 # Numba typed list!
 iou_thresholds = numba.typed.List()
 
@@ -263,316 +225,54 @@ def calculate_final_score(all_predictions, score_threshold):
         scores = scores[indexes]
 
         image_precision = calculate_image_precision(gt_boxes, pred_boxes,thresholds=iou_thresholds,form='pascal_voc')
-        #print('ap:', image_precision, 'iou:', iou_thresholds)
         final_scores.append(image_precision)
 
     return np.mean(final_scores)
 
-def calculate_final_score_old(all_predictions, score_threshold, form='pascal_voc'):
-    final_scores = []
-    for i in range(len(all_predictions)):
-        
-        #boxes, scores, labels = run_wbf(all_predictions, image_index=i)
-        
-        gt_boxes = all_predictions[i]['gt_boxes'].copy()
-        pred_boxes = all_predictions[i]['pred_boxes'].copy() #boxes.copy() #
-        scores = all_predictions[i]['scores'].copy() # scores.copy() #
-        image_id = all_predictions[i]['image_id']
+#############################################################
+import torch
 
-        indexes = np.where(scores>score_threshold)
-        pred_boxes = pred_boxes[indexes]
-        scores = scores[indexes]
-        
-        #print('gt boxes:', gt_boxes)
-        #print('pred boxes:', pred_boxes)
-        image_precision = calculate_image_precision(gt_boxes, pred_boxes,thresholds=iou_thresholds,form=form)
-        final_scores.append(image_precision)
+def predict_eval_set(model, validation_loader):
+    all_predictions = []
 
-    return np.mean(final_scores)
+    for images, targets, image_ids in tqdm(validation_loader, total=len(validation_loader)):
+        with torch.no_grad():
+            images = torch.stack(images)
+            images = images.cuda().float()
+            det = model(images, torch.tensor([1]*images.shape[0]).float().cuda())
 
-def eval_ap(model, val_loader):
-    all_predictions = predict_eval_set(model, val_loader)
-    
-    ap = calculate_final_score(all_predictions, 0.3)
-    
-    #metrics = {}
-    #thresholds = [0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6]
-    
-    #best_final_score, best_score_threshold = 0, 0
-    #for score_threshold in tqdm(np.arange(0, 1, 0.01), total=np.arange(0, 1, 0.01).shape[0]):
-    #    final_score = calculate_final_score(all_predictions, score_threshold)
-    #    if score_threshold in thresholds:
-    #        metrics[score_threshold] = final_score
-    #    if final_score > best_final_score:
-    #        best_final_score = final_score
-    #        best_score_threshold = score_threshold
-    
-    #metrics[best_score_threshold] = best_final_score
-    #metrics['best'] = best_final_score
-    
-    return ap
-
-
-
-################################ test #######################################
-from torch.utils.data import Dataset,DataLoader
-from torch.utils.data.sampler import SequentialSampler, RandomSampler
-import albumentations as A
-from albumentations.pytorch.transforms import ToTensorV2
-
-def get_train_transforms():
-    return A.Compose(
-        [
-            A.RandomSizedCrop(min_max_height=(800, 800), height=1024, width=1024, p=0.5),
-            A.OneOf([
-                A.HueSaturationValue(hue_shift_limit=0.2, sat_shift_limit= 0.2, 
-                                     val_shift_limit=0.2, p=0.9),
-                A.RandomBrightnessContrast(brightness_limit=0.2, 
-                                           contrast_limit=0.2, p=0.9),
-            ],p=0.9),
-            A.ToGray(p=0.01),
-            A.HorizontalFlip(p=0.5),
-            A.VerticalFlip(p=0.5),
-            A.Resize(height=512, width=512, p=1),
-            A.Cutout(num_holes=8, max_h_size=64, max_w_size=64, fill_value=0, p=0.5),
-            ToTensorV2(p=1.0),
-        ], 
-        p=1.0, 
-        bbox_params=A.BboxParams(
-            format='pascal_voc',
-            min_area=0, 
-            min_visibility=0,
-            label_fields=['labels']
-        )
-    )
-
-def get_valid_transforms():
-    return A.Compose(
-        [
-            A.Resize(height=512, width=512, p=1.0),
-            ToTensorV2(p=1.0),
-        ], 
-        p=1.0, 
-        bbox_params=A.BboxParams(
-            format='pascal_voc',
-            min_area=0, 
-            min_visibility=0,
-            label_fields=['labels']
-        )
-    )
-
-class DatasetRetriever(Dataset):
-
-    def __init__(self, marking, image_ids, transforms=None, test=False):
-        super().__init__()
-
-        self.image_ids = image_ids
-        self.marking = marking
-        self.transforms = transforms
-        self.test = test
-
-    def __getitem__(self, index: int):
-        image_id = self.image_ids[index]
-        
-        if self.test:
-            image, boxes = self.load_image_and_boxes(index)
-        else:
-            if random.random() > 0.5:
-                image, boxes = self.load_mixup_image_and_boxes(index)
-            else:
-                image, boxes = self.load_cutmix_image_and_boxes(index)
-
-        # there is only one class
-        labels = torch.ones((boxes.shape[0],), dtype=torch.int64)
-        
-        target = {}
-        target['boxes'] = boxes
-        target['labels'] = labels
-        target['image_id'] = torch.tensor([index])
-
-        if self.transforms:
-            for i in range(10):
-                sample = self.transforms(**{
-                    'image': image,
-                    'bboxes': target['boxes'],
-                    'labels': labels
+            for i in range(images.shape[0]):
+                boxes = det[i].detach().cpu().numpy()[:,:4]    
+                scores = det[i].detach().cpu().numpy()[:,4]
+                boxes[:, 2] = boxes[:, 2] + boxes[:, 0]
+                boxes[:, 3] = boxes[:, 3] + boxes[:, 1]
+                
+                gt_boxes = (targets[i]['boxes'].cpu().numpy()*2).clip(min=0, max=1023).astype(int)
+                if hasattr(validation_loader.dataset, 'yxyx') and validation_loader.dataset.yxyx is True:
+                    gt_boxes[:, [0,1,2,3]] = gt_boxes[:, [1,0,3,2]]
+                
+                all_predictions.append({
+                    'pred_boxes': (boxes*2).clip(min=0, max=1023).astype(int),
+                    'scores': scores,
+                    'gt_boxes': gt_boxes,
+                    'image_id': image_ids[i],
                 })
-                if len(sample['bboxes']) > 0:
-                    image = sample['image']
-                    target['boxes'] = torch.stack(tuple(map(torch.tensor, zip(*sample['bboxes'])))).permute(1, 0)
-                    target['boxes'][:,[0,1,2,3]] = target['boxes'][:,[1,0,3,2]]  #yxyx: be warning
-                    break
+    return all_predictions
 
-        return image, target, image_id
-
-    def __len__(self) -> int:
-        return self.image_ids.shape[0]
-
-    def load_image_and_boxes(self, index):
-        image_id = self.image_ids[index]
-        image = cv2.imread(f'{TRAIN_ROOT_PATH}/{image_id}.jpg', cv2.IMREAD_COLOR)
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB).astype(np.float32)
-        image /= 255.0
-        records = self.marking[self.marking['image_id'] == image_id]
-        #print(records)
-        boxes = records[['x', 'y', 'w', 'h']].values
-        boxes[:, 2] = boxes[:, 0] + boxes[:, 2]
-        boxes[:, 3] = boxes[:, 1] + boxes[:, 3]
-        #print('>>>boxes:', boxes)
-        #print('image shape:', image.shape)
-        return image, boxes
+def eval_metrics(model, validation_loader):
+    all_predictions = predict_eval_set(model, validation_loader)
     
-    def load_mixup_image_and_boxes(self, index):
-        img, boxes = self.load_image_and_boxes(index)
-        rindex = random.randint(0, self.image_ids.shape[0] - 1)
-        rimg, rboxes = self.load_image_and_boxes(rindex)
-        
-        mixup_image = (img + rimg)/2
-        mixup_boxes = np.concatenate([boxes, rboxes], 0)
-        #print(boxes.shape, rboxes.shape, mixup_boxes.shape)
-        
-        return mixup_image, mixup_boxes
-        
+    metrics = {}
+    score_thresholds = [0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6]
+    best_final_score, best_score_threshold = 0, 0
+    for score_threshold in tqdm(np.arange(0, 1, 0.01), total=np.arange(0, 1, 0.01).shape[0]):
+        final_score = calculate_final_score(all_predictions, score_threshold)
+        if score_threshold in score_thresholds: 
+            metrics[score_threshold] = round(final_score, 5)
+        if final_score > best_final_score:
+            best_final_score = final_score
+            best_score_threshold = score_threshold
 
-    def load_cutmix_image_and_boxes(self, index, imsize=1024):
-        """ 
-        This implementation of cutmix author:  https://www.kaggle.com/nvnnghia 
-        Refactoring and adaptation: https://www.kaggle.com/shonenkov
-        """
-        w, h = imsize, imsize
-        s = imsize // 2
-    
-        xc, yc = [int(random.uniform(imsize * 0.25, imsize * 0.75)) for _ in range(2)]  # center x, y
-        indexes = [index] + [random.randint(0, self.image_ids.shape[0] - 1) for _ in range(3)]
-
-        result_image = np.full((imsize, imsize, 3), 1, dtype=np.float32)
-        result_boxes = []
-
-        for i, index in enumerate(indexes):
-            image, boxes = self.load_image_and_boxes(index)
-            if i == 0:
-                x1a, y1a, x2a, y2a = max(xc - w, 0), max(yc - h, 0), xc, yc  # xmin, ymin, xmax, ymax (large image)
-                x1b, y1b, x2b, y2b = w - (x2a - x1a), h - (y2a - y1a), w, h  # xmin, ymin, xmax, ymax (small image)
-            elif i == 1:  # top right
-                x1a, y1a, x2a, y2a = xc, max(yc - h, 0), min(xc + w, s * 2), yc
-                x1b, y1b, x2b, y2b = 0, h - (y2a - y1a), min(w, x2a - x1a), h
-            elif i == 2:  # bottom left
-                x1a, y1a, x2a, y2a = max(xc - w, 0), yc, xc, min(s * 2, yc + h)
-                x1b, y1b, x2b, y2b = w - (x2a - x1a), 0, max(xc, w), min(y2a - y1a, h)
-            elif i == 3:  # bottom right
-                x1a, y1a, x2a, y2a = xc, yc, min(xc + w, s * 2), min(s * 2, yc + h)
-                x1b, y1b, x2b, y2b = 0, 0, min(w, x2a - x1a), min(y2a - y1a, h)
-            result_image[y1a:y2a, x1a:x2a] = image[y1b:y2b, x1b:x2b]
-            padw = x1a - x1b
-            padh = y1a - y1b
-
-            boxes[:, 0] += padw
-            boxes[:, 1] += padh
-            boxes[:, 2] += padw
-            boxes[:, 3] += padh
-
-            result_boxes.append(boxes)
-
-        result_boxes = np.concatenate(result_boxes, 0)
-        np.clip(result_boxes[:, 0:], 0, 2 * s, out=result_boxes[:, 0:])
-        result_boxes = result_boxes.astype(np.int32)
-        result_boxes = result_boxes[np.where((result_boxes[:,2]-result_boxes[:,0])*(result_boxes[:,3]-result_boxes[:,1]) > 0)]
-        return result_image, result_boxes
-
-def get_train_val_datasets(fold=0):
-    train_dataset = DatasetRetriever(
-        image_ids=df_folds[df_folds['fold'] != fold].index.values,
-        marking=marking,
-        transforms=get_train_transforms(),
-        test=False,
-    )
-    print(df_folds[df_folds['fold'] == fold].index.values[:10])
-    validation_dataset = DatasetRetriever(
-        image_ids=df_folds[df_folds['fold'] == fold].index.values,
-        marking=marking,
-        transforms=get_valid_transforms(),
-        test=True,
-    )
-    return train_dataset, validation_dataset
-
-def collate_fn(batch):
-    return tuple(zip(*batch))
-
-def load_net(checkpoint_path):
-    config = get_efficientdet_config('tf_efficientdet_d5')
-    net = EfficientDet(config, pretrained_backbone=False)
-
-    config.num_classes = 1
-    config.image_size=512
-    net.class_net = HeadNet(config, num_outputs=config.num_classes, norm_kwargs=dict(eps=.001, momentum=.01))
-
-    checkpoint = torch.load(checkpoint_path)
-    net.load_state_dict(checkpoint['model_state_dict'])
-
-    del checkpoint
-    gc.collect()
-
-    net = DetBenchEval(net, config)
-    net.eval();
-    return net.cuda()
-
-if __name__ == '__main__':
-    import sys
-    import torch
-    import torch.nn as nn
-    import gc
-    
-    sys.path.insert(0, "../../efficientdet-pytorch")
-    sys.path.insert(0, "../../omegaconf")
-    sys.path.insert(0, "../weightedboxesfusion")
-    from ensemble_boxes import *
-    
-    from effdet import get_efficientdet_config, EfficientDet, DetBenchEval
-    from effdet.efficientdet import HeadNet
-    from sklearn.model_selection import StratifiedKFold
-
-    
-    net = load_net('./ed5-mixup/best-fold-0.pth')
-    
-    DATA_DIR = '/relevance2-nfs/chec/data/wheat'
-    TRAIN_ROOT_PATH = f'{DATA_DIR}/train'
-    
-    marking = pd.read_csv(f'{DATA_DIR}/train.csv')
-
-    bboxs = np.stack(marking['bbox'].apply(lambda x: np.fromstring(x[1:-1], sep=',')))
-    for i, column in enumerate(['x', 'y', 'w', 'h']):
-        marking[column] = bboxs[:,i]
-    marking.drop(columns=['bbox'], inplace=True)
-    
-    skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
-
-    df_folds = marking[['image_id']].copy()
-    df_folds.loc[:, 'bbox_count'] = 1
-    df_folds = df_folds.groupby('image_id').count()
-    df_folds.loc[:, 'source'] = marking[['image_id', 'source']].groupby('image_id').min()['source']
-    df_folds.loc[:, 'stratify_group'] = np.char.add(
-        df_folds['source'].values.astype(str),
-        df_folds['bbox_count'].apply(lambda x: f'_{x // 15}').values.astype(str)
-    )
-    df_folds.loc[:, 'fold'] = 0
-
-    for fold_number, (train_index, val_index) in enumerate(skf.split(X=df_folds.index, y=df_folds['stratify_group'])):
-        df_folds.loc[df_folds.iloc[val_index].index, 'fold'] = fold_number
-        
-    train_dataset, validation_dataset = get_train_val_datasets(0)
-
-
-    val_loader = torch.utils.data.DataLoader(
-        validation_dataset, 
-        batch_size=4,
-        num_workers=4,
-        shuffle=False,
-        sampler=SequentialSampler(validation_dataset),
-        pin_memory=False,
-        collate_fn=collate_fn,
-    )
-    
-    print(len(val_loader), len(validation_dataset))
-    
-    print(eval_ap(net, val_loader))
-    
+        metrics['best_score'] = round(best_final_score, 5)
+        metrics['best_threshold'] = round(best_score_threshold, 5)
+    return metrics
